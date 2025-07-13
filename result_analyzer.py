ecd6ab343914bf43af2cfd0d1d1c9313e04d0921
eshanshahk <eshanshahk123@gmail.com>
2025-07-13 12:33:08 +0530

    sample manual list done

--- result_analyzer.py
+++ result_analyzer.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
-
 def get_details(no_of_students):
       for i in range(no_of_students):
             Name=input("Enter name : ")
             Class=int(input("Enter class : "))
-            students.append(Name)
-            students.append(Class)
+            students.append({'Name': Name, 'Class': Class})
 
+            
 students=[]
 no_of_students=int(input("Enter no of students : "))
 get_details(no_of_students)
